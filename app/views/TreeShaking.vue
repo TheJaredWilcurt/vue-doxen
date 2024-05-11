f42385dcbd11e49bb429dd299271ed3e0be88b3d
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2024-05-11 08:26:00 -0400

    Create Tree Shaking page

---
+++ app/views/TreeShaking.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="docs-page">
+    <h1
+      id="tree-shaking"
+      class="docs-title"
+    >
+      Tree Shaking
+    </h1>
+
+    <DocumentationSection id="" title="">
+      <p>
+      </p>
+
+      <CodeBox
+        :code="API"
+        :styleTokens="styleTokens"
+      />
+    </DocumentationSection>
+  </div>
+</template>
+
+<script>
+import { styleTokens } from '@/helpers/props.js';
+
+import DocumentationSection from '@@@/components/DocumentationSection.vue';
+
+const API = `
+<VueDoxenCustom
+   v-model="selected"
+  :demos="demos"
+  :options="{
+  }"
+  :styletokens="tokens"
+/>
+`.trim();
+
+export default {
+  name: 'TreeShaking',
+  components: {
+    DocumentationSection
+  },
+  props: {
+    styleTokens
+  },
+  constants: {
+    API
+  }
+};
+</script>
