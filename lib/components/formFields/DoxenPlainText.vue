3e7463d3773b3404e327d72ae166b44bf34f2f27
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2024-05-15 07:45:28 -0400

    simplify plain text, add codebox

--- lib/components/formFields/DoxenPlainText.vue
+++ lib/components/formFields/DoxenPlainText.vue
@@ -4,14 +4,20 @@
     :styleTokens="styleTokens"
   >
     <FormFieldLabel
-      :disabled="disabled"
+      :disabled="false"
       :errorMessage="errorMessage"
       :idFor="idFor"
       :label="label"
-      :required="required"
+      :required="false"
+      :styleTokens="styleTokens"
+    />
+    <CodeBox
+      v-if="codeAsString"
+      :code="codeAsString"
       :styleTokens="styleTokens"
     />
     <div
+      v-else
       v-text="modelValue"
       v-bind="applyStyleTokens({ doxenPlainText: true })"
     ></div>
@@ -31,47 +37,46 @@ import {
   createImportStatement
 } from '@/helpers/componentHelpers.js';
 import {
-  createDisabledProp,
   createErrorMessageProp,
   createMessageProp,
   createModelValueProp,
-  createOptionsProp,
   label,
-  required,
   styleTokens
 } from '@/helpers/props.js';
 
 import applyStyleTokens from '@/mixins/applyStyleTokensMixin.js';
 
+import CodeBox from '@/components/CodeBox.vue';
 import FormFieldFooter from '@/components/formFields/FormFieldFooter.vue';
 import FormFieldLabel from '@/components/formFields/FormFieldLabel.vue';
 import FormFieldsetWrapper from '@/components/formFields/FormFieldsetWrapper.vue';
 
 const COMPONENT_NAME = 'DoxenPlainText';
-const disabled = createDisabledProp('radio buttons');
-const errorMessage = createErrorMessageProp('radio buttons');
-const message = createMessageProp('radio buttons');
+const errorMessage = createErrorMessageProp('plain text');
+const message = createMessageProp('plain text');
 const modelValue = createModelValueProp([String, Number, Boolean, Object, Array]);
-const options = createOptionsProp(COMPONENT_NAME);
 
 export default {
   ...createImportStatement(COMPONENT_NAME),
   name: COMPONENT_NAME,
   components: {
+    CodeBox,
     FormFieldFooter,
     FormFieldLabel,
     FormFieldsetWrapper
   },
   mixins: [applyStyleTokens],
   props: {
-    disabled,
     errorMessage,
     label,
     message,
     modelValue,
-    options,
-    required,
-    styleTokens
+    styleTokens,
+    codeAsString: {
+      description: 'String of a code example to display instead of the raw modelValue.',
+      type: String,
+      default: undefined
+    }
   },
   computed: {
     uniqueId: function () {
