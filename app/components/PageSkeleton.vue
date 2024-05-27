df91113d8c57342a95a47be7850b40c527911521
TheJaredWilcurt <TheJaredWilcurt@users.noreply.github.com>
2024-05-27 00:59:06 -0400

    Skeleton hover

--- app/components/PageSkeleton.vue
+++ app/components/PageSkeleton.vue
@@ -1,62 +1,199 @@
 <template>
-  <div class="page">
-    <div class="blocks header"></div>
-    <div class="blocks description"></div>
-    <div class="component-container">
-      <div class="hr"></div>
-      <div class="blocks component"></div>
-      <div class="hr"></div>
-    </div>
-    <div class="props-playground">
-      <div class="props-1">
-        <div
-          v-for="prop in 4"
-          class="blocks prop"
-          :key="'prop' + prop"
-        ></div>
-      </div>
-      <div class="props-2">
-        <div class="blocks prop"></div>
-        <div class="slots-playground">
+  <div>
+    <div class="page">
+      <a
+        class="flex zone"
+        :class="{ 'zone-hover': hover === 'Component Name' }"
+        href="#name"
+        @mouseover="hover = 'Component Name'"
+        @mouseout="hoverOff"
+        @focus="hover = 'Component Name'"
+        @blur="hoverOff"
+      >
+        <div class="blocks header">
+          <span class="sr-only">Component Name</span>
+        </div>
+      </a>
+      <a
+        :class="{ 'zone-hover': hover === 'Component Description' }"
+        href="#description"
+        @mouseover="hover = 'Component Description'"
+        @mouseout="hoverOff"
+        @focus="hover = 'Component Description'"
+        @blur="hoverOff"
+      >
+        <div class="zone">
+          <div class="blocks description">
+            <span class="sr-only">Component Description</span>
+          </div>
+        </div>
+      </a>
+      <a
+        :class="{ 'zone-hover': hover === 'Import Statement' }"
+        href="#import"
+        @mouseover="hover = 'Import Statement'"
+        @mouseout="hoverOff"
+        @focus="hover = 'Import Statement'"
+        @blur="hoverOff"
+      >
+        <div class="zone">
+          <div class="import-statement">
+            <span class="sr-only">Import Statement</span>
+          </div>
+        </div>
+      </a>
+      <RouterLink
+        :class="{ 'zone-hover': hover === 'Your Component' }"
+        :to="{ name: 'demoFiles', hash: '#demo-files' }"
+        @mouseover="hover = 'Your Component'"
+        @mouseout="hoverOff"
+        @focus="hover = 'Your Component'"
+        @blur="hoverOff"
+      >
+        <div class="zone component-container">
+          <div class="hr"></div>
+          <div class="blocks component">
+            <span class="sr-only">Your Component</span>
+          </div>
+          <div class="hr"></div>
+        </div>
+      </RouterLink>
+      <div class="props-playground">
+        <a
+          href="#props"
+          @mouseover="hover = 'Props Playground'"
+          @mouseout="hoverOff"
+          @focus="hover = 'Props Playground'"
+          @blur="hoverOff"
+        >
           <div
-            v-for="slot in 2"
-            class="blocks slot"
-            :key="'slot' + slot"
+            class="props-1 props-zone"
+            :class="{ 'props-hover': hover === 'Props Playground' }"
+          >
+            <div
+              v-for="prop in 4"
+              class="blocks prop"
+              :key="'prop' + prop"
+            >
+              <span class="sr-only">Props Playground</span>
+            </div>
+          </div>
+        </a>
+        <div class="props-2">
+          <a
+            class="props-zone"
+            :class="{ 'props-hover': hover === 'Props Playground' }"
+            href="#props"
+            @mouseover="hover = 'Props Playground'"
+            @mouseout="hoverOff"
+            @focus="hover = 'Props Playground'"
+            @blur="hoverOff"
+          >
+            <div class="blocks prop">
+              <span class="sr-only">Props Playground</span>
+            </div>
+          </a>
+          <a
+            class="zone slots-playground"
+            :class="{ 'zone-hover': hover === 'Slots Playground' }"
+            href="#slots"
+            @mouseover="hover = 'Slots Playground'"
+            @mouseout="hoverOff"
+            @focus="hover = 'Slots Playground'"
+            @blur="hoverOff"
+          >
+            <div
+              v-for="slot in 2"
+              class="blocks slot"
+              :class="'slot-' + slot"
+              :key="'slot' + slot"
+            >
+              <span class="sr-only">Slots Playground</span>
+            </div>
+          </a>
+          <a
+            class="zone emit-logger-container"
+            :class="{ 'zone-hover': hover === 'Emit Logger' }"
+            href="#emits"
+            @mouseover="hover = 'Emit Logger'"
+            @mouseout="hoverOff"
+            @focus="hover = 'Emit Logger'"
+            @blur="hoverOff"
+          >
+            <div class="blocks emit-logger">
+              <span class="sr-only">Emit Logger</span>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div
+        class="zone live-code"
+        :class="{ 'live-code-hover': hover === 'Live Code' }"
+        role="button"
+        tabindex="0"
+        @focus="hover = 'Live Code'"
+        @blur="hoverOff"
+        @mouseover="hover = 'Live Code'"
+        @mouseout="hoverOff"
+      >
+        <div class="buttons-container">
+          <span class="sr-only">Live Code</span>
+          <div
+            v-for="button in 2"
+            class="blocks button"
+            :key="'button' + button"
           ></div>
         </div>
-        <div class="blocks emit-logger"></div>
-      </div>
-    </div>
-    <div class="live-code">
-      <div class="buttons-container">
-        <div
-          v-for="button in 2"
-          class="blocks button"
-          :key="'button' + button"
-        ></div>
-      </div>
-      <div class="code-box">
-        <div
-          v-for="codeLine in 4"
-          class="code-line"
-          :key="'codeLine' + codeLine"
-        >
-          <TransitionGroup name="code-blocks">
-            <div
-              v-for="codeBlock in randomizeCodeBlocks(skeletonKey)"
-              class="code-block"
-              :style="(
-                'width:' + randomizeCodeBlockWidth(skeletonKey) + 'px;' +
-                'background: hsl(0, 0%, ' + randomizeCodeBlockColor(skeletonKey) + '%);'
-              )"
-              :key="'codeBlock' + codeBlock"
-            ></div>
-          </TransitionGroup>
+        <div class="code-box">
+          <div
+            v-for="codeLine in 4"
+            class="code-line"
+            :key="'codeLine' + codeLine"
+          >
+            <TransitionGroup name="code-blocks">
+              <div
+                v-for="codeBlock in randomizeCodeBlocks(skeletonKey)"
+                class="code-block"
+                :style="(
+                  'width:' + randomizeCodeBlockWidth(skeletonKey) + 'px;' +
+                  'background: hsl(0, 0%, ' + randomizeCodeBlockColor(skeletonKey) + '%);'
+                )"
+                :key="'codeBlock' + codeBlock"
+              ></div>
+            </TransitionGroup>
+          </div>
         </div>
       </div>
+      <a
+        class="flex zone"
+        :class="{ 'zone-hover': hover === 'Props Documentation' }"
+        href="#props"
+        @mouseover="hover = 'Props Documentation'"
+        @mouseout="hoverOff"
+        @focus="hover = 'Props Documentation'"
+        @blur="hoverOff"
+      >
+        <div class="blocks props-documentation">
+          <span class="sr-only">Props Documentation</span>
+        </div>
+      </a>
+      <a
+        class="flex zone"
+        :class="{ 'zone-hover': hover === 'Emits Documentation' }"
+        href="#emits"
+        @mouseover="hover = 'Emits Documentation'"
+        @mouseout="hoverOff"
+        @focus="hover = 'Emits Documentation'"
+        @blur="hoverOff"
+      >
+        <div class="blocks emits-documentation">
+          <span class="sr-only">Emits Documentation</span>
+        </div>
+      </a>
     </div>
-    <div class="blocks props-documentation"></div>
-    <div class="blocks slots-documentation"></div>
+    <strong>
+      {{ hover || '&nbsp;' }}
+    </strong>
   </div>
 </template>
 
@@ -65,6 +202,7 @@ export default {
   name: 'PageSkeleton',
   data: function () {
     return {
+      hover: '',
       codeBlocksAmount: 0,
       codeBlockWidth: 0,
       codeBlockColor: 0,
@@ -73,6 +211,9 @@ export default {
     };
   },
   methods: {
+    hoverOff: function () {
+      this.hover = '';
+    },
     randomizeCodeBlocks: function () {
       return Math.round(Math.random() * 4) + 1;
     },
@@ -98,23 +239,31 @@ export default {
 .page {
   background: #EEE;
   width: 300px;
-  padding: 10px;
+  border-radius: 7px;
+  padding: 10px 7px;
 }
 .blocks {
   background: #CCC;
   border-radius: 7px;
-  margin: 7px 0px;
 }
 .header {
+  width: 100%;
   height: 30px;
 }
 .description {
   width: 190px;
   height: 10px;
 }
+.import-statement {
+  width: 120px;
+  height: 10px;
+  background: #737373;
+  border-radius: 7px;
+}
 .component {
   width: 100px;
   height: 40px;
+  margin: 7px 0px;
 }
 .hr {
   height: 1px;
@@ -145,6 +294,12 @@ export default {
 .slots-playground {
   flex-grow: 1;
   justify-content: space-evenly;
+}
+.slot-1 {
+  margin-right: 3.5px;
+}
+.slot-2 {
+  margin-left: 3.5px;
 }
 .buttons-container {
   display: flex;
@@ -177,8 +332,64 @@ export default {
   margin: 1px 4px 0px;
   transition: 1s ease all;
 }
-.props-documentation {}
-.slots-documentation {}
+.props-documentation {
+  width: 100%;
+  height: 80px;
+}
+.emits-documentation {
+  width: 100%;
+  height: 40px;
+}
+
+.props-zone,
+.zone {
+  padding: 3.5px 3.5px;
+}
+.live-code-hover,
+.props-hover,
+.zone-hover,
+.zone:hover {
+  background: #A7A7A7;
+}
+.flex {
+  display: flex;
+}
+.zone-hover {
+  display: flex;
+}
+.props-1.props-hover {
+  border-radius: 7px 7px 7px 0px;
+}
+.props-2 .props-hover {
+  position: relative;
+  border-radius: 0px 0px 7px 7px;
+}
+.props-2 .props-hover:before {
+  content: '';
+  position: absolute;
+  top: 0px;
+  right: -7px;
+  display: block;
+  background: #A7A7A7;
+  width: 7px;
+  height: 7px;
+}
+.props-2 .props-hover:after {
+  content: '';
+  position: absolute;
+  top: 0px;
+  right: -7px;
+  display: block;
+  background: #EEE;
+  width: 7px;
+  height: 7px;
+  border-radius: 7px 0px 0px 0px;
+}
+
+.zone.slots-playground,
+.zone.emit-logger-container {
+  border-radius: 7px;
+}
 
 .code-blocks-enter-active,
 .code-blocks-leave-active,
@@ -190,10 +401,15 @@ export default {
 .code-blocks-leave-to {
   opacity: 0.0;
 }
-.props-documentation {
-  height: 80px;
-}
-.slots-documentation {
-  height: 40px;
+
+.sr-only {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0px;
+  overflow: hidden;
+  clip: rect(0,0,0,0);
+  white-space: nowrap;
+  border: 0px;
 }
 </style>
