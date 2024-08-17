fdb3a81cfd1f8a28e68adfa5f2b259051636711a
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2024-08-17 13:00:06 -0400

    create markup tests

--- tests/unit/lib/helpers/demoHelpers.test.js
+++ tests/unit/lib/helpers/demoHelpers.test.js
@@ -21,6 +21,7 @@ import {
 
 import {
   combinePropsAndPropsToDemo,
+  createMarkupExample,
   getSlotDataFromComponent
 } from '@/helpers/demoHelpers.js';
 
@@ -322,6 +323,246 @@ describe('Demo helpers', () => {
     });
   });
 
+  describe('createMarkupExample', () => {
+    test('With nothing passed in', () => {
+      expect(createMarkupExample())
+        .toEqual('<ComponentName />');
+    });
+
+    test('Sort attributes, props, and emits', () => {
+      const tag = 'DummyComponent';
+      const attributes = [
+        {
+          name: 'name',
+          required: true,
+          value: 'Bob'
+        },
+        {
+          name: 'percent',
+          default: 0,
+          value: 20
+        },
+        {
+          name: 'firstName',
+          default: undefined,
+          value: 'Bob'
+        },
+        {
+          name: 'enabled',
+          default: false,
+          value: true
+        },
+        {
+          name: 'modelValue',
+          default: undefined,
+          value: 'text'
+        }
+      ];
+      const slots = {};
+      const emits = [
+        'update:modelValue',
+        'click',
+        'update:firstName',
+        'submit'
+      ];
+
+      expect(createMarkupExample(tag, attributes, slots, emits))
+        .toEqual([
+          '<DummyComponent',
+          '  v-model="yourDataValue"',
+          '  v-model:firstName="yourDataValue"',
+          '  :enabled="true"',
+          '  name="Bob"',
+          '  :percent="20"',
+          '  @click="yourMethodName"',
+          '  @submit="yourMethodName"',
+          '/>'
+        ].join('\n'));
+    });
+
+    describe('Tag', () => {
+      test('Has a tag', () => {
+        expect(createMarkupExample('DummyComponent'))
+          .toEqual('<DummyComponent />');
+      })
+    });
+
+    describe('Attributes', () => {
+      test('Has model-value', () => {
+        const tag = '';
+        const attributes = [
+          {
+            name: 'model-value'
+          }
+        ];
+
+        expect(createMarkupExample(tag, attributes))
+          .toEqual([
+            '<ComponentName',
+            '  v-model="yourDataValue"',
+            '/>'
+          ].join('\n'));
+      });
+
+      test('Has modelValue', () => {
+        const tag = '';
+        const attributes = [
+          {
+            name: 'modelValue'
+          }
+        ];
+
+        expect(createMarkupExample(tag, attributes))
+          .toEqual([
+            '<ComponentName',
+            '  v-model="yourDataValue"',
+            '/>'
+          ].join('\n'));
+      });
+
+      test('String attribute has default value', () => {
+        const tag = '';
+        const attributes = [
+          {
+            name: 'label',
+            default: 'Label',
+            value: 'Label'
+          }
+        ];
+
+        expect(createMarkupExample(tag, attributes))
+          .toEqual('<ComponentName />');
+      });
+
+      test('String attribute has non-default value', () => {
+        const tag = '';
+        const attributes = [
+          {
+            name: 'label',
+            default: 'Label',
+            value: 'Name'
+          }
+        ];
+
+        expect(createMarkupExample(tag, attributes))
+          .toEqual([
+            '<ComponentName',
+            '  label="Name"',
+            '/>'
+          ].join('\n'));
+      });
+
+      test('String attribute has non-default non-value', () => {
+        const tag = '';
+        const attributes = [
+          {
+            name: 'label',
+            default: 'Label',
+            value: ''
+          }
+        ];
+
+        expect(createMarkupExample(tag, attributes))
+          .toEqual('<ComponentName />');
+      });
+
+      test('Dynamic attribute has non-default value', () => {
+        const tag = '';
+        const attributes = [
+          {
+            name: 'disabled',
+            default: false,
+            value: true
+          }
+        ];
+
+        expect(createMarkupExample(tag, attributes))
+          .toEqual([
+            '<ComponentName',
+            '  :disabled="true"',
+            '/>'
+          ].join('\n'));
+      });
+    });
+
+    describe('Slots', () => {
+      test('Has default slot', () => {
+        const tag = '';
+        const attributes = [];
+        const slots = {
+          default: '<h1>Text</h1>'
+        };
+
+        expect(createMarkupExample(tag, attributes, slots))
+          .toEqual([
+            '<ComponentName>',
+            '  <h1>Text</h1>',
+            '</ComponentName>'
+          ].join('\n'));
+      });
+
+      test('Has named slots', () => {
+        const tag = '';
+        const attributes = [];
+        const slots = {
+          header: '<h1>Title</h1>',
+          footer: '<footer>\n  <button>Submit</button>\n</footer>'
+        };
+
+        expect(createMarkupExample(tag, attributes, slots))
+          .toEqual([
+            '<ComponentName>',
+            '  <template #header>',
+            '    <h1>Title</h1>',
+            '  </template>',
+            '  <template #footer>',
+            '    <footer>',
+            '      <button>Submit</button>',
+            '    </footer>',
+            '  </template>',
+            '</ComponentName>'
+          ].join('\n'));
+      });
+    });
+
+    describe('Emits', () => {
+      test('Click event', () => {
+        const tag = '';
+        const attributes = [];
+        const slots = {};
+        const emits = [
+          'click'
+        ];
+
+        expect(createMarkupExample(tag, attributes, slots, emits))
+          .toEqual([
+            '<ComponentName',
+            '  @click="yourMethodName"',
+            '/>'
+          ].join('\n'));
+      });
+
+      test('Named v-model', () => {
+        const tag = '';
+        const attributes = [
+          {
+            name: 'firstName'
+          }
+        ];
+        const slots = {};
+        const emits = [
+          'update:firstName'
+        ];
+
+        expect(createMarkupExample(tag, attributes, slots, emits))
+          .toEqual([
+            '<ComponentName',
+            '  v-model:firstName="yourDataValue"',
+            '/>'
+          ].join('\n'));
+      });
+    });
+  });
   describe('getSlotDataFromComponent', () => {
     const defaultSlot = {
       default: {
