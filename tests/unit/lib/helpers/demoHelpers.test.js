7edda029ef49f2779d00d9027448398b4389823d
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2024-08-17 22:43:53 -0400

    autoGeneratePlaygroundProps tests

--- tests/unit/lib/helpers/demoHelpers.test.js
+++ tests/unit/lib/helpers/demoHelpers.test.js
@@ -20,6 +20,7 @@ import {
 } from '@/library.js';
 
 import {
+  autoGeneratePlaygroundProps,
   combinePropsAndPropsToDemo,
   createMarkupExample,
   getSlotDataFromComponent,
@@ -31,6 +32,431 @@ import DummyScriptSetupApi from '@@@/components/DummyScriptSetupApi.vue';
 import DummySlots from '@@@/components/DummySlots.vue';
 
 describe('Demo helpers', () => {
+  describe('autoGeneratePlaygroundProps', () => {
+    let components;
+    let styleTokens;
+
+    beforeEach(() => {
+      components = {
+        checkbox: { template: 'DummyCheckbox' },
+        dropdown: { template: 'DummyDropdown' },
+        emitLog: { template: 'DummyEmitLog' },
+        emitsDocumentation: { template: 'DummyEmitsDocumentation' },
+        header: { template: 'DummyHeader' },
+        jsonTextarea: { template: 'DummyJsonTextarea' },
+        numberField: { template: 'DummyNumberField' },
+        plainText: { template: 'DummyPlainText' },
+        propsDocumentation: { template: 'DummyPropsDocumentation' },
+        radioDials: { template: 'DummyRadioDials' },
+        rangeSlider: { template: 'DummyRangeSlider' },
+        textField: { template: 'DummyTextField' },
+        textarea: { template: 'DummyTextarea' }
+      };
+      styleTokens = { button: 'button' };
+    });
+
+    test('No arguments', () => {
+      expect(autoGeneratePlaygroundProps())
+        .toEqual({});
+    });
+
+    test('No props', () => {
+      const props = {};
+
+      expect(autoGeneratePlaygroundProps(props, components, styleTokens))
+        .toEqual({});
+    });
+
+    test('Prop with custom component', () => {
+      const props = {
+        amount: {
+          type: Number,
+          component: { template: 'CustomComponent' },
+          props: { label: 'Label' }
+        }
+      };
+
+      expect(autoGeneratePlaygroundProps(props, components, styleTokens))
+        .toEqual(props);
+    });
+
+    test('Prop is v-model', () => {
+      const props = {
+        modelValue: {
+          type: String
+        }
+      };
+
+      expect(autoGeneratePlaygroundProps(props, components, styleTokens))
+        .toEqual({
+          modelValue: {
+            component: components.plainText,
+            props: {
+              asCode: true,
+              label: 'Model Value',
+              styleTokens
+            }
+          }
+        });
+    });
+
+    test('Prop is a modelModifiers', () => {
+      const props = {
+        modelModifiers: {
+          type: String
+        }
+      };
+
+      expect(autoGeneratePlaygroundProps(props, components, styleTokens))
+        .toEqual({
+          modelModifiers: {
+            component: components.jsonTextarea,
+            props: {
+              label: 'Model Modifiers',
+              styleTokens
+            }
+          }
+        });
+    });
+
+    test('Prop is message', () => {
+      const props = {
+        someMessages: {}
+      };
+
+      expect(autoGeneratePlaygroundProps(props, components, styleTokens))
+        .toEqual({
+          someMessages: {
+            component: components.textarea,
+            props: {
+              label: 'Some Messages',
+              styleTokens
+            }
+          }
+        });
+    });
+
+    test('Prop is notes', () => {
+      const props = {
+        someNotes: {}
+      };
+
+      expect(autoGeneratePlaygroundProps(props, components, styleTokens))
+        .toEqual({
+          someNotes: {
+            component: components.textarea,
+            props: {
+              label: 'Some Notes',
+              styleTokens
+            }
+          }
+        });
+    });
+
+    test('Prop is comment', () => {
+      const props = {
+        someComments: {}
+      };
+
+      expect(autoGeneratePlaygroundProps(props, components, styleTokens))
+        .toEqual({
+          someComments: {
+            component: components.textarea,
+            props: {
+              label: 'Some Comments',
+              styleTokens
+            }
+          }
+        });
+    });
+
+    test('Prop needs range slider', () => {
+      const props = {
+        amount: {
+          type: Number,
+          min: 20,
+          max: 30
+        }
+      };
+
+      expect(autoGeneratePlaygroundProps(props, components, styleTokens))
+        .toEqual({
+          amount: {
+            component: components.rangeSlider,
+            props: {
+              label: 'Amount',
+              modelValue: undefined,
+              min: 20,
+              max: 30,
+              styleTokens
+            }
+          }
+        });
+    });
+
+    test('Prop is a number', () => {
+      const props = {
+        amount: {
+          type: Number
+        }
+      };
+
+      expect(autoGeneratePlaygroundProps(props, components, styleTokens))
+        .toEqual({
+          amount: {
+            component: components.numberField,
+            props: {
+              label: 'Amount',
+              modelValue: undefined,
+              styleTokens
+            }
+          }
+        });
+    });
+
+    test('Props has a few allowed values', () => {
+      const props = {
+        color: {
+          allowed: [
+            'red',
+            'blue'
+          ]
+        }
+      };
+
+      expect(autoGeneratePlaygroundProps(props, components, styleTokens))
+        .toEqual({
+          color: {
+            component: components.radioDials,
+            props: {
+              label: 'Color',
+              modelValue: undefined,
+              options: [
+                {
+                  name: 'Red',
+                  value: 'red'
+                },
+                {
+                  name: 'Blue',
+                  value: 'blue'
+                }
+              ],
+              styleTokens
+            }
+          }
+        });
+    });
+
+    test('Props has many allowed values', () => {
+      const props = {
+        color: {
+          allowed: [
+            'red',
+            'blue',
+            'green',
+            'purple',
+            'yellow'
+          ]
+        }
+      };
+
+      expect(autoGeneratePlaygroundProps(props, components, styleTokens))
+        .toEqual({
+          color: {
+            component: components.dropdown,
+            props: {
+              label: 'Color',
+              modelValue: undefined,
+              options: [
+                {
+                  name: 'Red',
+                  value: 'red'
+                },
+                {
+                  name: 'Blue',
+                  value: 'blue'
+                },
+                {
+                  name: 'Green',
+                  value: 'green'
+                },
+                {
+                  name: 'Purple',
+                  value: 'purple'
+                },
+                {
+                  name: 'Yellow',
+                  value: 'yellow'
+                }
+              ],
+              styleTokens
+            }
+          }
+        });
+    });
+
+    test('Prop is a boolean', () => {
+      const props = {
+        disabled: {
+          type: Boolean
+        }
+      };
+
+      expect(autoGeneratePlaygroundProps(props, components, styleTokens))
+        .toEqual({
+          disabled: {
+            component: components.checkbox,
+            props: {
+              name: 'Disabled',
+              modelValue: false,
+              styleTokens
+            }
+          }
+        });
+    });
+
+    test('Prop is a required array', () => {
+      const props = {
+        colors: {
+          type: Array,
+          required: true
+        }
+      };
+
+      expect(autoGeneratePlaygroundProps(props, components, styleTokens))
+        .toEqual({
+          colors: {
+            component: components.jsonTextarea,
+            props: {
+              label: 'Colors',
+              modelValue: [],
+              styleTokens
+            }
+          }
+        });
+    });
+
+    test('Prop is a required object', () => {
+      const props = {
+        user: {
+          type: Object,
+          required: true
+        }
+      };
+
+      expect(autoGeneratePlaygroundProps(props, components, styleTokens))
+        .toEqual({
+          user: {
+            component: components.jsonTextarea,
+            props: {
+              label: 'User',
+              modelValue: {},
+              styleTokens
+            }
+          }
+        });
+    });
+
+    test('Prop is a required object or array', () => {
+      const props = {
+        user: {
+          type: [Object, Array],
+          required: true
+        }
+      };
+
+      expect(autoGeneratePlaygroundProps(props, components, styleTokens))
+        .toEqual({
+          user: {
+            component: components.jsonTextarea,
+            props: {
+              label: 'User',
+              modelValue: {},
+              styleTokens
+            }
+          }
+        });
+    });
+
+    test('Prop is a function', () => {
+      const props = {
+        action: {
+          type: Function
+        }
+      };
+
+      expect(autoGeneratePlaygroundProps(props, components, styleTokens))
+        .toEqual({
+          action: {
+            component: components.plainText,
+            props: {
+              label: 'Action',
+              styleTokens
+            }
+          }
+        });
+    });
+
+    test('Prop is a required function', () => {
+      const props = {
+        action: {
+          type: Function,
+          required: true
+        }
+      };
+
+      expect(autoGeneratePlaygroundProps(props, components, styleTokens))
+        .toEqual({
+          action: {
+            component: components.plainText,
+            props: {
+              label: 'Action',
+              modelValue: expect.any(Function),
+              styleTokens
+            }
+          }
+        });
+    });
+
+    test('Catch all for props', () => {
+      const props = {
+        name: {}
+      };
+
+      expect(autoGeneratePlaygroundProps(props, components, styleTokens))
+        .toEqual({
+          name: {
+            component: components.textField,
+            props: {
+              label: 'Name',
+              modelValue: 'Name',
+              styleTokens
+            }
+          }
+        });
+    });
+
+    test('Catch all for props with default', () => {
+      const props = {
+        name: {
+          default: 'Text'
+        }
+      };
+
+      expect(autoGeneratePlaygroundProps(props, components, styleTokens))
+        .toEqual({
+          name: {
+            component: components.textField,
+            props: {
+              label: 'Name',
+              modelValue: 'Text',
+              styleTokens
+            }
+          }
+        });
+    });
+  });
+
   describe('combinePropsAndPropsToDemo', () => {
     test('No props or propsToDemo', () => {
       const propsToDemo = undefined;
