3419e347aa711cd30e884bc1c1d60a4f8f4f152b
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2024-08-29 09:01:31 -0400

    tests

--- tests/unit/lib/components/doxenCodeBox.test.js
+++ tests/unit/lib/components/doxenCodeBox.test.js
@@ -10,15 +10,18 @@ describe('DoxenCodeBox', () => {
   // Props
   const html = '<div attribute="value">\n  Text\n</div>';
   const js = 'const value = 4;\nconsole.log({\n  key: value\n});';
+  const code = '<h1>Text</h1>';
   const styleTokens = styleTokensBuiltIn;
   const requiredProps = {};
 
   beforeEach(() => {
     console.error = vi.fn();
+    vi.useFakeTimers();
   });
 
   afterEach(() => {
     console.error = consoleError;
+    vi.useRealTimers();
   });
 
   const setupWrapper = async (props) => {
@@ -59,5 +62,79 @@ describe('DoxenCodeBox', () => {
 
     expect(wrapper)
       .toMatchSnapshot();
+  });
+
+  test('Copy text temporarily displayed after clicking on codebox', async () => {
+    const wrapper = await setupWrapper({ code });
+    const firstDiv = wrapper.find('[aria-label="Code example"]');
+    await firstDiv.trigger('click');
+
+    expect(wrapper.vm.copied)
+      .toEqual(true);
+
+    vi.runAllTimers();
+
+    expect(wrapper.vm.copied)
+      .toEqual(false);
+  });
+
+  test('Copy text temporarily displayed after pressing enter', async () => {
+    const wrapper = await setupWrapper({ code });
+    const firstDiv = wrapper.find('[aria-label="Code example"]');
+    await firstDiv.trigger('keydown.enter');
+
+    expect(wrapper.vm.copied)
+      .toEqual(true);
+
+    vi.runAllTimers();
+
+    expect(wrapper.vm.copied)
+      .toEqual(false);
+  });
+
+  test('Copy text temporarily displayed after pressing space', async () => {
+    const wrapper = await setupWrapper({ code });
+    const firstDiv = wrapper.find('[aria-label="Code example"]');
+    await firstDiv.trigger('keydown.space');
+
+    expect(wrapper.vm.copied)
+      .toEqual(true);
+
+    vi.runAllTimers();
+
+    expect(wrapper.vm.copied)
+      .toEqual(false);
+  });
+
+  describe('Mock clipboard', () => {
+    let consoleError;
+    let writeText;
+
+    beforeEach(() => {
+      consoleError = console.error;
+      console.error = vi.fn();
+
+      writeText = navigator.clipboard.writeText;
+      navigator.clipboard.writeText = vi.fn(() => {
+        throw new Error('Clipboard permission missing.');
+      });
+    });
+
+    afterEach(() => {
+      console.error = consoleError;
+      navigator.clipboard.writeText = writeText;
+    });
+
+    test('Catch copy error.', async () => {
+      const wrapper = await setupWrapper({ code });
+      const firstDiv = wrapper.find('[aria-label="Code example"]');
+      await firstDiv.trigger('click');
+
+      expect(navigator.clipboard.writeText)
+        .toHaveBeenCalledWith(code);
+
+      expect(console.error)
+        .toHaveBeenCalledWith('Clipboard permission missing.');
+    });
   });
 });
