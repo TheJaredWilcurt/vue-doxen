9fcbd59daa4238e02592491fe7d0fdbd05f893eb
The Jared Wilcurt <4629794+TheJaredWilcurt@users.noreply.github.com>
2025-08-16 20:44:24 -0400

    TDD

---
+++ tests/unit/lib/components/htmlFragments.test.js
@@ -0,0 +1,50 @@
+import HtmlFragments from '@/components/HtmlFragments.vue';
+
+import testHelpers from '@@/unit/testHelpers.js';
+
+describe('HtmlFragments.vue', () => {
+  beforeEach(() => {
+    globalThis.vueSnapshots.removeComments = true;
+  });
+
+  const setupWrapper = async (html) => {
+    const options = {
+      props: {
+        html: html || ''
+      }
+    };
+    const wrapper = await testHelpers.mount(HtmlFragments, options);
+    return wrapper;
+  };
+
+  test('Renders nothing', async () => {
+    const wrapper = await setupWrapper();
+
+    expect(wrapper)
+      .toMatchSnapshot();
+  });
+
+  test('Renders one element', async () => {
+    const wrapper = await setupWrapper('<div>Text</div>');
+
+    expect(wrapper)
+      .toMatchSnapshot();
+  });
+
+  test('Renders two root elements', async () => {
+    const wrapper = await setupWrapper([
+      '<div>Text</div>',
+      '<p>Paragraph</p>'
+    ].join(''));
+
+    expect(wrapper)
+      .toMatchSnapshot();
+  });
+
+  test('Renders one text node', async () => {
+    const wrapper = await setupWrapper('Some Text');
+
+    expect(wrapper)
+      .toMatchSnapshot();
+  });
+});
