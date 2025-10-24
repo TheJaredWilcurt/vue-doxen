d9a6b81897b13efd2d55fc9b2c380b9997e8df45
The Jared Wilcurt <4629794+TheJaredWilcurt@users.noreply.github.com>
2025-10-24 09:40:59 -0400

    EmitsMustHaveExample added

---
+++ tests/unit/lib/linter/rules/demos/allEmitsMustHave/example.test.js
@@ -0,0 +1,264 @@
+import { allEmitsMustHaveExample } from '@/linter/rules/demos/allEmitsMustHave/example.js';
+
+describe('Emits must have an example', () => {
+  const consoleInfo = console.info;
+  const demoName = 'MyComponent';
+  const emitName = 'myEmit';
+  let options;
+  let linterSettings;
+  let errors;
+
+  beforeEach(() => {
+    console.info = vi.fn();
+    options = {};
+    linterSettings = {
+      demos: {
+        allEmitsMustHave: {
+          example: true
+        }
+      }
+    };
+    errors = [];
+  });
+
+  afterEach(() => {
+    console.info = consoleInfo;
+  });
+
+  describe('Rule', () => {
+    describe('Emits and emitsToDemo as objects', () => {
+      test('Catches the missing example', () => {
+        const demos = {
+          [demoName]: {
+            emitsToDemo: {
+              [emitName]: {}
+            }
+          }
+        };
+        allEmitsMustHaveExample.rule(demos, options, linterSettings, errors);
+
+        expect(console.info)
+          .toHaveBeenCalledWith('The ' + demoName + ' emit ' + emitName + ' must have an example.');
+
+        expect(errors)
+          .toEqual([demoName]);
+      });
+
+      test('Catches missing emit from emitstoDemo', () => {
+        const demos = {
+          [demoName]: {
+            emits: {
+              [emitName]: {}
+            }
+          }
+        };
+        allEmitsMustHaveExample.rule(demos, options, linterSettings, errors);
+
+        expect(console.info)
+          .toHaveBeenCalledWith('The ' + demoName + ' emit ' + emitName + ' must have an example.');
+
+        expect(errors)
+          .toEqual([demoName]);
+      });
+
+      test('Passes when example provided', () => {
+        const demos = {
+          [demoName]: {
+            emitsToDemo: {
+              [emitName]: {
+                example: '@' + emitName + '="myMethod"'
+              }
+            }
+          }
+        };
+        allEmitsMustHaveExample.rule(demos, options, linterSettings, errors);
+
+        expect(console.info)
+          .not.toHaveBeenCalled();
+
+        expect(errors)
+          .toEqual([]);
+      });
+
+      test('Passes when example is explicitly set to undefined', () => {
+        const demos = {
+          [demoName]: {
+            emitsToDemo: {
+              [emitName]: {
+                example: undefined
+              }
+            }
+          }
+        };
+        allEmitsMustHaveExample.rule(demos, options, linterSettings, errors);
+
+        expect(console.info)
+          .not.toHaveBeenCalled();
+
+        expect(errors)
+          .toEqual([]);
+      });
+
+      describe('Example inside component', () => {
+        test('Catches the missing example', () => {
+          const demos = {
+            [demoName]: {
+              component: {
+                emitsToDemo: {
+                  [emitName]: {}
+                }
+              }
+            }
+          };
+          allEmitsMustHaveExample.rule(demos, options, linterSettings, errors);
+
+          expect(console.info)
+            .toHaveBeenCalledWith('The ' + demoName + ' emit ' + emitName + ' must have an example.');
+
+          expect(errors)
+            .toEqual([demoName]);
+        });
+
+        test('Catches missing emit from emitstoDemo', () => {
+          const demos = {
+            [demoName]: {
+              component: {
+                emits: {
+                  [emitName]: {}
+                }
+              }
+            }
+          };
+          allEmitsMustHaveExample.rule(demos, options, linterSettings, errors);
+
+          expect(console.info)
+            .toHaveBeenCalledWith('The ' + demoName + ' emit ' + emitName + ' must have an example.');
+
+          expect(errors)
+            .toEqual([demoName]);
+        });
+
+        test('Passes when example provided', () => {
+          const demos = {
+            [demoName]: {
+              component: {
+                emitsToDemo: {
+                  [emitName]: {
+                    example: '@' + emitName + '="myMethod"'
+                  }
+                }
+              }
+            }
+          };
+          allEmitsMustHaveExample.rule(demos, options, linterSettings, errors);
+
+          expect(console.info)
+            .not.toHaveBeenCalled();
+
+          expect(errors)
+            .toEqual([]);
+        });
+
+        test('Passes when example is explicitly set to undefined', () => {
+          const demos = {
+            [demoName]: {
+              component: {
+                emitsToDemo: {
+                  [emitName]: {
+                    example: undefined
+                  }
+                }
+              }
+            }
+          };
+          allEmitsMustHaveExample.rule(demos, options, linterSettings, errors);
+
+          expect(console.info)
+            .not.toHaveBeenCalled();
+
+          expect(errors)
+            .toEqual([]);
+        });
+      });
+    });
+
+    describe('Emits and emitsToDemo as arrays', () => {
+      test('Catches the missing example', () => {
+        const demos = {
+          [demoName]: {
+            emitsToDemo: [emitName]
+          }
+        };
+        allEmitsMustHaveExample.rule(demos, options, linterSettings, errors);
+
+        expect(console.info)
+          .toHaveBeenCalledWith('The ' + demoName + ' emit ' + emitName + ' must have an example.');
+
+        expect(errors)
+          .toEqual([demoName]);
+      });
+
+      test('Catches missing emit from emitstoDemo', () => {
+        const demos = {
+          [demoName]: {
+            emits: [emitName]
+          }
+        };
+        allEmitsMustHaveExample.rule(demos, options, linterSettings, errors);
+
+        expect(console.info)
+          .toHaveBeenCalledWith('The ' + demoName + ' emit ' + emitName + ' must have an example.');
+
+        expect(errors)
+          .toEqual([demoName]);
+      });
+
+      describe('Description inside component', () => {
+        test('Catches the missing example', () => {
+          const demos = {
+            [demoName]: {
+              component: {
+                emitsToDemo: [emitName]
+              }
+            }
+          };
+          allEmitsMustHaveExample.rule(demos, options, linterSettings, errors);
+
+          expect(console.info)
+            .toHaveBeenCalledWith('The ' + demoName + ' emit ' + emitName + ' must have an example.');
+
+          expect(errors)
+            .toEqual([demoName]);
+        });
+
+        test('Catches missing emit from emitstoDemo', () => {
+          const demos = {
+            [demoName]: {
+              component: {
+                emits: [emitName]
+              }
+            }
+          };
+          allEmitsMustHaveExample.rule(demos, options, linterSettings, errors);
+
+          expect(console.info)
+            .toHaveBeenCalledWith('The ' + demoName + ' emit ' + emitName + ' must have an example.');
+
+          expect(errors)
+            .toEqual([demoName]);
+        });
+      });
+    });
+  });
+
+  test('Documentation', () => {
+    expect(!!allEmitsMustHaveExample.description)
+      .toEqual(true);
+
+    expect(!!allEmitsMustHaveExample.url)
+      .toEqual(true);
+
+    expect(!!allEmitsMustHaveExample.examples.length)
+      .toEqual(true);
+  });
+});
