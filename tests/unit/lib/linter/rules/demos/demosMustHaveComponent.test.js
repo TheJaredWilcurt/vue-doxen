754da475fb03761f4859630d84206316aafd814c
The Jared Wilcurt <4629794+TheJaredWilcurt@users.noreply.github.com>
2025-10-28 21:19:17 -0400

    Demos must have component lint rule

---
+++ tests/unit/lib/linter/rules/demos/demosMustHaveComponent.test.js
@@ -0,0 +1,110 @@
+import { demosMustHaveComponent } from '@/linter/rules/demos/demosMustHaveComponent.js';
+
+import DoxenButton from '@/components/DoxenButton.vue';
+
+describe('Components must have a name', () => {
+  const consoleInfo = console.info;
+  const demoName = 'MyComponent';
+  const key = 'component';
+  const message = 'The ' + demoName + ' demo must have a component.';
+  let options;
+  let linterSettings;
+  let errors;
+
+  beforeEach(() => {
+    console.info = vi.fn();
+    options = {};
+    linterSettings = {
+      demos: {
+        demosMustHaveComponent: true
+      }
+    };
+    errors = [];
+  });
+
+  afterEach(() => {
+    console.info = consoleInfo;
+  });
+
+  describe('Rule', () => {
+    test('Catches the missing key', () => {
+      const demos = {
+        [demoName]: {}
+      };
+      demosMustHaveComponent.rule(demos, options, linterSettings, errors);
+
+      expect(console.info)
+        .toHaveBeenCalledWith(message);
+
+      expect(errors)
+        .toEqual([demoName]);
+    });
+
+    test('Fails when key is undefined', () => {
+      const demos = {
+        [demoName]: {
+          [key]: undefined
+        }
+      };
+      demosMustHaveComponent.rule(demos, options, linterSettings, errors);
+
+      expect(console.info)
+        .toHaveBeenCalledWith(message);
+
+      expect(errors)
+        .toEqual([demoName]);
+    });
+
+    test('Passes when key is component', () => {
+      const demos = {
+        [demoName]: {
+          [key]: DoxenButton
+        }
+      };
+      demosMustHaveComponent.rule(demos, options, linterSettings, errors);
+
+      expect(console.info)
+        .not.toHaveBeenCalled();
+
+      expect(errors)
+        .toEqual([]);
+    });
+
+    test('Passes when demo is component', () => {
+      const demos = {
+        [demoName]: DoxenButton
+      };
+      demosMustHaveComponent.rule(demos, options, linterSettings, errors);
+
+      expect(console.info)
+        .not.toHaveBeenCalled();
+
+      expect(errors)
+        .toEqual([]);
+    });
+  });
+
+  test('Documentation', () => {
+    function validateRuleDocumentation (ruleDefinition) {
+      const existsWithValue = (
+        !!ruleDefinition.title.length &&
+        !!ruleDefinition.description.length &&
+        !!ruleDefinition.url.length &&
+        !!Object.keys(ruleDefinition.examples).length &&
+        !!ruleDefinition.examples['Demo File'].length
+      );
+      const hasCorrectTypes = (
+        typeof(ruleDefinition.title) === 'string' &&
+        typeof(ruleDefinition.description) === 'string' &&
+        typeof(ruleDefinition.url) === 'string' &&
+        typeof(ruleDefinition.examples) === 'object' &&
+        !Array.isArray(ruleDefinition.examples) &&
+        typeof(ruleDefinition.examples['Demo File']) === 'string'
+      );
+      return existsWithValue && hasCorrectTypes;
+    }
+
+    expect(validateRuleDocumentation(demosMustHaveComponent))
+      .toEqual(true);
+  });
+});
