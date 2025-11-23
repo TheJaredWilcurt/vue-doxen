089a8963c02dd0541a0625cd5abce1faa448405d
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2025-11-23 08:56:40 -0500

    Indentation

--- tests/unit/lib/components/formFields/doxenCheckbox.test.js
+++ tests/unit/lib/components/formFields/doxenCheckbox.test.js
@@ -5,60 +5,64 @@ import DoxenCheckbox from '@/components/formFields/DoxenCheckbox.vue';
 import testHelpers from '@@/unit/testHelpers.js';
 
 describe('DoxenCheckbox.vue', () => {
-    const styleTokens = styleTokensBuiltIn;
-    const requiredProps = {};
+  const styleTokens = styleTokensBuiltIn;
+  const requiredProps = {};
 
-    const setupWrapper = async (props) => {
-        const options = {
-            props: {
-                ...requiredProps,
-                ...props
-            },
-            slots: {
-                default: 'Slot Text'
-            }
-        };
-        const wrapper = await testHelpers.mount(DoxenCheckbox, options);
-        return wrapper;
+  const setupWrapper = async (props) => {
+    const options = {
+      props: {
+        ...requiredProps,
+        ...props
+      },
+      slots: {
+        default: 'Slot Text'
+      }
     };
+    const wrapper = await testHelpers.mount(DoxenCheckbox, options);
+    return wrapper;
+  };
 
-    test('Renders correctly', async () => {
-        const wrapper = await setupWrapper();
+  test('Renders correctly', async () => {
+    const wrapper = await setupWrapper();
 
-        expect(wrapper)
-            .toMatchSnapshot();
-    });
+    expect(wrapper)
+      .toMatchSnapshot();
+  });
 
-    test('Style tokens', async () => {
-        const wrapper = await setupWrapper({ styleTokens });
+  test('Style tokens', async () => {
+    const wrapper = await setupWrapper({ styleTokens });
 
-        expect(wrapper)
-            .toMatchSnapshot();
-    });
+    expect(wrapper)
+      .toMatchSnapshot();
+  });
 
-    test('Emit click', async () => {
-        const wrapper = await setupWrapper();
+  test('Emit click', async () => {
+    const wrapper = await setupWrapper();
 
-        await wrapper.find('input').trigger('click');
+    await wrapper.find('input').trigger('click');
 
-        expect(wrapper.emitted().click[0].length)
-            .toEqual(1);
-    });
+    expect(wrapper.emitted().click[0].length)
+      .toEqual(1);
+  });
 
-    test('idFor should equal input name', async () => {
-        const wrapper = await setupWrapper();
+  test('idFor should equal input name', async () => {
+    const wrapper = await setupWrapper();
 
-        const checkboxInput = wrapper.find('input[type="checkbox"]');
-        const labelElement = wrapper.find('label');
+    const checkboxInput = wrapper.find('input[type="checkbox"]');
+    const labelElement = wrapper.find('label');
+
+    // Ensure both elements exist
+    expect(checkboxInput.exists())
+      .toBe(true);
+
+    expect(labelElement.exists())
+      .toBe(true);
     
-        // Ensure both elements exist
-        expect(checkboxInput.exists()).toBe(true);
-        expect(labelElement.exists()).toBe(true);
-    
-        const checkboxName = checkboxInput.attributes('name');
-        const labelFor = labelElement.attributes('for');
-    
-        // Compare the attributes
-        expect(checkboxName).toEqual(labelFor);
-    })
-})
+    const checkboxName = checkboxInput.attributes('name');
+    const labelFor = labelElement.attributes('for');
+
+    // Compare the attributes
+    expect(checkboxName)
+      .toEqual(labelFor);
+  });
+});
