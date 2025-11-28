a7bbf987fb07d1ebd595a4067e3e4fba0d06a90c
The Jared Wilcurt <4629794+TheJaredWilcurt@users.noreply.github.com>
2025-11-28 10:03:41 -0500

    FormFieldFooter tests

---
+++ tests/unit/lib/components/formFields/formFieldFooter.test.js
@@ -0,0 +1,56 @@
+import { styleTokensBuiltIn } from '@/helpers/styleTokens.js';
+
+import FormFieldFooter from '@/components/formFields/FormFieldFooter.vue';
+
+import testHelpers from '@@/unit/testHelpers.js';
+
+describe('FormFieldFooter.vue', () => {
+  // Props
+  const errorMessage = 'Error';
+  const innerHTML = '<strong>Markup</strong>'
+  const message = 'Message';
+  const styleTokens = styleTokensBuiltIn;
+  const requiredProps = {};
+
+  const setupWrapper = async (props) => {
+    const options = {
+      props: {
+        ...requiredProps,
+        ...props
+      },
+      slots: {
+        default: 'Slot text'
+      }
+    };
+    const wrapper = await testHelpers.mount(FormFieldFooter, options);
+    return wrapper;
+  };
+
+  test('Renders correctly with nothing passed in', async () => {
+    const wrapper = await setupWrapper({});
+
+    expect(wrapper)
+      .toMatchSnapshot();
+  });
+
+  test('Renders message', async () => {
+    const wrapper = await setupWrapper({ message, styleTokens });
+
+    expect(wrapper)
+      .toMatchSnapshot();
+  });
+
+  test('Renders errorMessage', async () => {
+    const wrapper = await setupWrapper({ errorMessage, styleTokens });
+
+    expect(wrapper)
+      .toMatchSnapshot();
+  });
+
+  test('Renders innerHTML', async () => {
+    const wrapper = await setupWrapper({ innerHTML, styleTokens });
+
+    expect(wrapper)
+      .toMatchSnapshot();
+  });
+});
